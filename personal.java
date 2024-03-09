70ae3aa110958034f9fa74be37092ba8f6e01ca5
Mittal <ritest1@gmail.com>
2024-03-09 12:59:48 +0000

    New Commit

---
+++ personal.java
@@ -0,0 +1,18 @@
+public class personal
+{
+    //creating static variables
+    static String name = "Mittal";
+    static String surname = "Gurjar";
+    static String age = "35";
+    static String salary = "£25,000";
+    static String mobile = "07923484921";
+    static String address = "56,Ringway Nn5 0sh";
+    // Main method
+    public static void main(String[] args) {
+        System.out.println("My Name is " + name + " " + surname + ".");
+        System.out.println("I am " + age + " old" + ".");
+        System.out.println("My salary is " + salary + ".");
+        System.out.println("My phone number is " + mobile + ".");
+        System.out.println("My address is " + address + ".");
+    }
+}
